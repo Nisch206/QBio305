c30c9f3810e423e0656a5512fd74360a9dec595c
Nisch206 <nicholas.schmitt.2212@googlemail.com>
2024-01-26 15:03:08 +0100

    Add Tahir's code for troubleshooting

---
+++ GWAS_manhatten_AnnotateSNP.R
@@ -0,0 +1,109 @@
+#########################
+#      Rscript          #
+#       GWAS            #
+#    Manhatten plot     #
+#########################
+
+# Load the library
+library(qqman)
+
+# Read GWAS output file created using gemma
+gwas_data_bio17 <- read.table("sample_project_GWAS2.assoc.txt", header = TRUE)
+
+# Make the Manhattan plot on the gwasResults dataset
+manhattan(gwas_data_bio17, chr="chr", bp="ps", snp="rs", p="p_wald" )
+
+# modify colour of points
+manhattan(x = gwas_data_bio17, chr = "chr", bp = "ps", p = "p_wald", snp = "rs", col = c("blue4", "orange3"), logp = TRUE)
+
+##########Bonferroni correction############
+
+# Make the Manhattan plot using FDR-corrected p-values
+pvals_bonf <- p.adjust(gwas_data_bio17$p_wald, method = "bonferroni")
+
+# Add FDR-corrected p-value threshold line to the plot
+pval_bonf <- 0.05  # Desired FDR threshold, you can adjust this if needed
+
+# Add the adjusted p-values as a new column to the data frame
+gwas_data_bio17$pvals_bonf <- pvals_bonf
+
+# Create the Manhattan plot with the new adjusted p-values column
+manhattan(gwas_data_bio17, chr = "chr", bp = "ps", snp = "rs", p = "pvals_bonf",
+          suggestiveline = -log10(pval_bonf), genomewideline = FALSE,
+          annotatePval = -log10(pval_bonf),
+          col = c("blue4", "orange3"))
+
+# Create the Manhattan plot with expanded datapoints on the y-axis
+manhattan(gwas_data_bio17, chr = "chr", bp = "ps", snp = "rs", p = "pvals_bonf",
+          suggestiveline = -log10(pvals_bonf), genomewideline = FALSE,
+          annotatePval = -log10(pval_bonf),
+          col = c("blue4", "orange3"),
+          ylim = c(0, max(-log10(pval_bonf)) + 5))  # Expand the y-axis range
+
+##########FDR correction############
+
+# Make the Manhattan plot using FDR-corrected p-values
+pvals_fdr <- p.adjust(gwas_data_bio17$p_wald, method = "BH")
+
+# Add FDR-corrected p-value threshold line to the plot
+pval_fdr <- 0.05  # Desired FDR threshold, you can adjust this if needed
+
+# Add the adjusted p-values as a new column to the data frame
+gwas_data_bio17$pvals_fdr <- pvals_fdr
+
+# Create the Manhattan plot with the new adjusted p-values column
+manhattan(gwas_data_bio17, chr = "chr", bp = "ps", snp = "rs", p = "pvals_fdr",
+          suggestiveline = -log10(pval_fdr), genomewideline = FALSE,
+          annotatePval = -log10(pval_fdr),
+          col = c("blue4", "orange3"))
+
+###
+# GWAS manhatten Plot & SNP annotation
+# using ggrepel, dplyr, ggplot
+###
+
+library(ggrepel)
+
+# Load the dplyr package
+library(dplyr)
+
+# Prepare the dataset
+gwas <- gwas_data_bio17 %>% 
+  # Compute chromosome size
+  group_by(chr) %>% 
+  summarise(chr_len=max(ps)) %>% 
+  # Calculate cumulative position of each chromosome
+  mutate(tot=cumsum(chr_len)-chr_len) %>%
+  dplyr::select(-chr_len) %>%
+  # Add this info to the initial dataset
+  left_join(gwas_data_bio17, ., by=c("chr"="chr")) %>%
+  # Add a cumulative position of each SNP
+  arrange(chr, ps) %>%
+  mutate(Chromosome=ps+tot,   # Fixed BPcum assignment
+         is_annotate=ifelse(-log10(pvals_fdr)>1.2, "yes", "no"))   # Changed 'pvals_fdr' to 'Pvals_fdr'
+
+# Prepare X axis
+axisdf <- gwas %>% group_by(chr) %>% summarize(center=( max(Chromosome) + min(Chromosome) ) / 2 )
+# extract labels column
+rs<-gwas$rs
+
+# plot using ggplot
+
+ggplot(gwas, aes(x=Chromosome, y=-log10(pvals_fdr))) +
+  geom_point(aes(color=as.factor(chr)), alpha=0.8, size=1.3) +
+  scale_color_manual(values = rep(c("black", "orange"), 22 )) +
+  scale_x_continuous(label = axisdf$chr, breaks= axisdf$center) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.8)), breaks = seq(0, 5, 0.5), 
+                     limits = c(0,1.5)) +
+  geom_label_repel(data=subset(gwas, is_annotate=="yes"), aes(label=rs), size=2) +
+  geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "red") +
+  theme_bw() +
+  theme( 
+    legend.position="none",
+    panel.border = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.text = element_text(size = 8)
+  )
+
